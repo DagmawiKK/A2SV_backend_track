ccfebbdcd4a65c168f022b97cdd20c0060e52783
DagmawiKK <dagmawikassa1@gmail.com>
2025-07-16 09:38:47 +0300

    Add task controller to handle HTTP requests

---
+++ task_4/task_manager/controllers/task_controller.go
@@ -0,0 +1,58 @@
+package controllers
+
+import (
+	"net/http"
+	"strconv"
+	"task_manager/data"
+	"task_manager/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetTasks(c *gin.Context) {
+	c.JSON(http.StatusOK, data.GetAllTasks())
+}
+
+func GetTask(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	task, err := data.GetTaskByID(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+		return
+	}
+	c.JSON(http.StatusOK, task)
+}
+
+func CreateTask(c *gin.Context) {
+	var task models.Task
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	created := data.CreateTask(task)
+	c.JSON(http.StatusCreated, created)
+}
+
+func UpdateTask(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	var task models.Task
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	updated, err := data.UpdateTask(id, task)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+		return
+	}
+	c.JSON(http.StatusOK, updated)
+}
+
+func DeleteTask(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	if err := data.DeleteTask(id); err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Task deleted"})
+}
